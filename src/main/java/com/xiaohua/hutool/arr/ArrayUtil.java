f3dc0d89d0ef14f3b555769e5697e220ec2affc8
2275698462 <2275698462@qq.com>
2023-11-06 11:42:21 +0800

    第一次提交

---
+++ src/main/java/com/xiaohua/hutool/arr/ArrayUtil.java
@@ -0,0 +1,9 @@
+package com.xiaohua.hutool.arr;
+
+/**
+ * @author illusion
+ * @date 2023/11/6 11:40
+ */
+public class ArrayUtil {
+
+}
---
+++ src/main/java/com/xiaohua/hutool/arr/PrimitiveArrayUtil.java
@@ -0,0 +1,9 @@
+package com.xiaohua.hutool.arr;
+
+/**
+ * @author illusion
+ * @date 2023/11/6 11:42
+ */
+public class PrimitiveArrayUtil {
+
+}
